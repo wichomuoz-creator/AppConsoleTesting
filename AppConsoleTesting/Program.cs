086949d60f95188b739c8e30dd88c60f000eed5e
wichomuoz-creator <wichomuoz@gmail.com>
2026-02-12 11:25:26 -0500

    Add new output line with name and fix duplicate line
    Modificación solicitada para la tarea

--- AppConsoleTesting/Program.cs
+++ AppConsoleTesting/Program.cs
@@ -5,3 +5,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Esto es una prueba de git v1");
 
 Console.WriteLine("Esto es una prueba de git v2");
+
+Console.WriteLine("Mi nombre es Wilson Muñoz");
